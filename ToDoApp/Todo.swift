54ab1927720a6ee64c98d4ea998a49dd600c4369
Abhishek Bhardwaj <=>
2024-01-08 13:59:48 +0530

    Create UI and Struct to store task

---
+++ ToDoApp/Todo.swift
@@ -0,0 +1,12 @@
+//
+//  Todo.swift
+//  ToDoApp
+//
+//  Created by Abhishek Bhardwaj on 08/01/24.
+//
+
+import Foundation
+struct Todo {
+    let title: String
+    let isComplete: Bool = false
+}
--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let todos = [
+        Todo(title: "Complete DSP Flow"),
+        Todo(title: "Eat Dinner"),
+        Todo(title: "Take Medicine"),
+        Todo(title: "Do Coding!")
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
